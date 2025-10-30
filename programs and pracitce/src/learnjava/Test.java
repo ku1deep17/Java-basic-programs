e448001a26fd90955d7688ccc5efb42253717481
Kuldeep <kp87947@gmail.com>
2025-10-30 11:22:39 +0530

    this is my second commit

---
+++ programs and pracitce/src/learnjava/Test.java
@@ -0,0 +1,9 @@
+package learnjava;
+
+public class Test {
+    public static void main(String [] args){
+        for(int a =1; a<=10; a++){
+            System.out.println("THIS IS NUMBER : "+ a);
+        }
+    }
+}
